721c97a8bc8395305904065dec73bc29b8a95264
Sean Burman <sb9443353@gmail.com>
2024-10-07 17:44:50 -0600

    Simplified badges component

--- src/components/languageBadges.tsx
+++ src/components/languageBadges.tsx
@@ -4,25 +4,38 @@ import { StyleSheet } from "@/app/types";
 import { Box } from "@mui/material";
 import Image from "next/image";
 
+const badges = [
+    "react",
+    "golang",
+    "javascript",
+    "typescript",
+    "python",
+    "html5",
+    "css3",
+    "mongodb",
+    "postgresql",
+    "bash",
+    "github",
+    "bitbucket",
+    "mui",
+    "figma",
+    "fitbit",
+];
+
 export default function LanguageBadges() {
 
     return(
         <Box style={styles.root}>
-            <Image style={styles.badge} src="/img/react.svg" alt="react" width={90} height={40}/>
-            <Image style={styles.badge} src="/img/golang.svg" alt="golang" width={100} height={40}/>
-            <Image style={styles.badge} src="/img/javascript.svg" alt="javascript" width={100} height={40}/>
-            <Image style={styles.badge} src="/img/typescript.svg" alt="typescript" width={100} height={40}/>
-            <Image style={styles.badge} src="/img/python.svg" alt="python" width={100} height={40}/>
-            <Image style={styles.badge} src="/img/html5.svg" alt="html5" width={100} height={40}/>
-            <Image style={styles.badge} src="/img/css3.svg" alt="css3" width={100} height={40}/>
-            <Image style={styles.badge} src="/img/mongodb.svg" alt="mongodb" width={100} height={40}/>
-            <Image style={styles.badge} src="/img/postgresql.svg" alt="postgresql" width={100} height={40}/>
-            <Image style={styles.badge} src="/img/bash.svg" alt="GNU bash" width={100} height={40}/>
-            <Image style={styles.badge} src="/img/github.svg" alt="GNU bash" width={100} height={40}/>
-            <Image style={styles.badge} src="/img/bitbucket.svg" alt="GNU bash" width={100} height={40}/>
-            <Image style={styles.badge} src="/img/mui.svg" alt="MUI" width={100} height={40}/>
-            <Image style={styles.badge} src="/img/figma.svg" alt="figma" width={100} height={40}/>
-            <Image style={styles.badge} src="/img/fitbit.svg" alt="fitbit" width={100} height={40}/>
+            {badges.map((badge) => (
+                <Image
+                    key={badge}
+                    src={`https://badges.pages.dev/badge/${badge}.svg`}
+                    alt={badge}
+                    style={styles.badge}
+                    width={100}
+                    height={100}
+                />
+            ))}
         </Box>
     )
 }
@@ -30,8 +43,6 @@ export default function LanguageBadges() {
 const styles: StyleSheet = {
     root: {
         display: "flex",
-        // justifyContent: "space-between",
-        // alignItems: "center",
         flexWrap: "wrap",
         padding: "1rem",
         gap: "0.5rem",
