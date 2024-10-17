78083ea22c7ff978d8b85b9d378621d3e0eac6b9
Sean Burman <sb9443353@gmail.com>
2024-10-17 11:44:28 -0600

    Added typing to alert message severity props

--- src/components/alert.tsx
+++ src/components/alert.tsx
@@ -1,9 +1,11 @@
 import { Alert } from "@mui/material";
 
+export type Severity = "error" | "warning" | "info" | "success";
+
 export default function AlertMessage(props: {
     message: string;
-    severity: string;
+    severity: Severity;
 }) {
     if (props.message === "") return null;
-    return <Alert severity={props.severity as any}>{props.message}</Alert>;
+    return <Alert severity={props.severity}>{props.message}</Alert>;
 }
