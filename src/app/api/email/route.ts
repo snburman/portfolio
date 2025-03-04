36d88bb63c3f1da707a0fc5ca5ff617ab1017324
Sean Burman <sb9443353@gmail.com>
2025-03-04 01:03:08 -0700

    Added mail error

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -5,6 +5,7 @@ import Mail from 'nodemailer/lib/mailer';
 export async function POST(req: NextRequest) {;
     const origin = req.headers.get('origin');
     if (origin !== process.env.NEXT_PUBLIC_API_URL) {
+        console.error("route: invalid origin: ", origin);
         return NextResponse.json({ success: false });
     }
 
